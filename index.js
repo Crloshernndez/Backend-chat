23a7aa45fc96f1bd186e804aa57488bb3af91666
carlos <hernandez1989carlos@gmail.com>
2021-01-13 12:22:39 -0500

    [create] network/routes.js file : create routes for all components. [update] index.js : import routes and connect with server app

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const config = require("./config");
 // inicializacion de express
 const app = express();
 
+//SERVER
 app.listen(config.port, () => {
   console.log(`App is working on ${config.host}:${config.port}`);
 });
+
+// ROUTES
+const router = require("./network/routes");
+router(app);
