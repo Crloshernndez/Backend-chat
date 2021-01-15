7df2693270c15e9fa989473498e6fe95f895827b
carlos <hernandez1989carlos@gmail.com>
2021-01-15 09:43:56 -0500

    [update] components/chat/network.js : create the routes for the chat comoponent s requests

--- components/chat/network.js
+++ components/chat/network.js
@@ -3,3 +3,42 @@ const responses = require("../../network/responses");
 const controller = require("./controller");
 
 const router = express.Router();
+
+router.get("/", getChats);
+router.post("/", addChat);
+router.delete("/:id", deleteChat);
+
+function getChats(req, res) {
+  const filterChat = req.query.id || null;
+
+  controller
+    .getChats(filterChat)
+    .then((chatList) => {
+      responses.success(req, res, chatList, 200);
+    })
+    .catch((e) => {
+      responses.error(req, res, e.message, 500);
+    });
+}
+
+function addChat(req, res) {
+  controller
+    .addChat(req.body.users)
+    .then((data) => {
+      responses.success(req, res, data, 201);
+    })
+    .catch((e) => {
+      responses.error(req, res, e.message, 500);
+    });
+}
+
+function deleteChat(req, res) {
+  controller
+    .deleteChat(req.params.id)
+    .then(() => {
+      responses.success(req, res, `chat ${req.params.id} delated`);
+    })
+    .catch((e) => {
+      responses.error(req, res, e.message, 500);
+    });
+}
